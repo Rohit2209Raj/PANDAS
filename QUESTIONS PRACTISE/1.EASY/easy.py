ddbcfd8265cbac9a266a24aab0082f3fddedf48c
Rohit Raj <rohitrajptn2209@gmail.com>
2026-01-12 23:22:30 +0530

    12 jan

--- QUESTIONS PRACTISE/1.EASY/easy.py
+++ QUESTIONS PRACTISE/1.EASY/easy.py
@@ -43,6 +43,9 @@ df = pd.read_csv(r"D:\COLLEGE\PANDAS\QUESTIONS PRACTISE\employee_data.csv")
 # 2. Select employees earning > 60,000 
 # 3. Select rows with index 5 to 10 
 # 4. Select employees in HR or IT
+'''
+    very very very good example ad questiosn
+'''
 
 # df2=df[['name','salary']]
 # print(df2.head())
@@ -50,7 +53,85 @@ df = pd.read_csv(r"D:\COLLEGE\PANDAS\QUESTIONS PRACTISE\employee_data.csv")
 # df2=df[df['salary']>60000]
 # print(df2.head())
 
+
+# df2=df[df['dept'].isin(['HR','IT'])]
+# print(df2)
+
+# df2=df.iloc[5:11]
+# print(df2)
+
+# Q4. Sorting & Ranking 
+# 1. Sort employees by salary (descending) 
+# 2. Rank employees by salary (dense ranking) 
+# 3. Find top 5 highest paid employees 
+
+'''
+GOOD AND GRETA LEARNING
+'''
+
+
+# df.sort_values(by="salary",ascending=False,inplace=True)
+# # print(df)
+
+# df['salary_rank']=(df['salary'].rank(method='dense',ascending=False))
 # print(df)
 
-df2=df[df['dept']=='IT' | df['dept']=='HR]']
-print(df2)
+# df2=df[df['salary_rank']<=5]
+# print(df2[['name','salary']])
+
+
+# # Given missing salaries and departments: 
+# # 1. Count missing values per column 
+# # 2. Fill salary NaNs with median 
+# # 3. Drop rows where department is missing 
+# # 4. Forward-fill remaining NaNs
+# print(df)
+# print()
+# print(df.isnull().sum())
+# print()
+# # df['salary'].fillna(df['salary'].mean(),inplace=True)
+# # df.dropna(subset=['salary'],inplace=True)
+# df = df.ffill()
+
+# print(df)
+
+
+# Q6. Value Counts & Unique 
+# 1. Count employees per department 
+# 2. Find number of unique departments 
+# 3. Get department names sorted alphabetically 
+# print(df)
+# print(len(df['dept'].unique()))
+
+# # df.sort_values(by="dept",ascending=True,inplace=True)
+# # print(df)
+
+# useful=df.groupby("dept")['emp_id'].count()
+# print(useful)
+
+'''
+coorect and better solutino
+# 1. Count employees per department
+print(df['dept'].value_counts())
+
+# 2. Number of unique departments
+print(df['dept'].nunique())
+
+# 3. Department names sorted alphabetically
+print(sorted(df['dept'].unique()))
+
+'''
+
+
+# Q7. Data Type Conversion 
+# 1. Convert join_date to datetime 
+# 2. Extract year, month, day 
+# 3. Convert salary to integer
+
+print(df.head())
+
+print(df['join_date'].dtype)
+print(df.head())
+
+
+
